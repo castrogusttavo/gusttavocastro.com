06badb1518d2342e4651e0616e364c4f14e0038f
Gusttavo Castro <gusttavoleandro2023@gmail.com>
2024-03-16 23:42:02 -0300

    criar o projeto
    Aqui estão todas as pastas e arquivos principais do projeto.

---
+++ data/projects.js
@@ -0,0 +1,82 @@
+const items = [
+  {
+    year: 'Future...',
+    projects: [
+      {
+        title: 'Oxygen',
+        active: true,
+      },
+      {
+        title: 'Stark',
+        active: true,
+      },
+      {
+        title: 'Pensamento do designer',
+        active: true,
+      },
+      {
+        title: 'Vintage app music',
+        active: true,
+      },
+      {
+        title: 'Social media for devs',
+        active: true,
+      },
+    ]
+  },
+  {
+    year: '2024',
+    projects: [
+      {
+        title: 'Softis',
+        description: 'Shaping the future, bit by bit.',
+        active: true,
+        icon: 'resend',
+        stats: 'Startup',
+      },
+      {
+        title: 'Teste IDs',
+        url: 'https://ids.saude360.app.br/entrada.php',
+        icon: 'palette',
+        active: true,
+      },
+      {
+        title: 'Therha',
+        description: 'Next generation of Operating System',
+        icon: 'palette',
+        stats: 'Startup',
+        icon: 'dracula-pro',
+        active: true,
+      },
+      {
+        title: 'Board',
+        url: 'https://https://next14-board-tutorial.vercel.app/',
+        icon: 'palette',
+        active: true,
+      },
+    ],
+  },
+  {
+    year: '2023',
+    projects: [
+      {
+        title: 'ClinWare',
+        description: 'Online Scheduling & EMR',
+        active: true,
+        icon: 'email',
+        stats: 'TCC',
+      },
+      {
+        title: 'NVEC',
+        description: 'Managing Personal Finances Smartly',
+        url: 'https://https://github.com/castrogusttavo/nvec_tcc',
+        icon: 'clipboard',
+        stats: 'TCC',
+        icon: 'clipboard-js',
+        active: true,
+      },
+    ],
+  },
+]
+
+export default items
