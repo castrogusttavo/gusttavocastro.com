90402babed119caeb6284df8114408ac8c28afa2
Gusttavo Castro <gusttavoleandro2023@gmail.com>
2024-03-18 20:58:47 -0300

    update email.js

--- pages/api/email.js
+++ pages/api/email.js
@@ -1,21 +1,22 @@
-import { Resend } from 'resend'
-const resend = new Resend(process.env.RESEND_API_KEY)
-import EmailTemplate from '../../components/EmailTemplate'
+import { Resend } from 'resend';
+import EmailTemplate from '../../components/EmailTemplate';
+
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export default async function sendEmail(req, res) {
   try {
-    const data = req.body
+    const { name, email, message } = req.body; // Desestruturação dos campos específicos
 
     await resend.sendEmail({
-      from: 'gusttavocastro.com <website@zenorocha.com>',
+      from: 'castrogusttavo.dev@gmail.com <castrogusttavo.dev@gmail.com>',
       to: 'castrogusttavo.dev@gmail.com',
-      replyTo: data.email,
-      subject: `${data.name} - via gusttavocastro..com`,
-      react: <EmailTemplate {...data} />,
-    })
+      replyTo: email, // Usando o email diretamente do req.body
+      subject: `${name} - via gusttavocastro.com`, // Usando o nome diretamente do req.body
+      html: EmailTemplate({ name, email, message }), // Passando os campos específicos para EmailTemplate
+    });
 
-    res.status(200).json({ message: 'Email sent' })
+    res.status(200).json({ message: 'Email sent' });
   } catch (e) {
-    res.status(500).json({ message: e.message })
+    res.status(500).json({ message: e.message });
   }
 }
