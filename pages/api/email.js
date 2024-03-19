8fe870b7f08b30035b0ba03a055b49033e21130f
Gusttavo Castro <gusttavoleandro2023@gmail.com>
2024-03-18 21:19:14 -0300

    update email.js

--- pages/api/email.js
+++ pages/api/email.js
@@ -1,14 +1,14 @@
 import { Resend } from 'resend';
 import EmailTemplate from '../../components/EmailTemplate';
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend('re_123456789');
 
 export default async function sendEmail(req, res) {
   try {
     const { name, email, message } = req.body; // Desestruturação dos campos específicos
 
     await resend.sendEmail({
-      from: 'castrogusttavo.dev@gmail.com <castrogusttavo.dev@gmail.com>',
+      from: 'Gusttavo <onboarding@resend.dev>',
       to: 'castrogusttavo.dev@gmail.com',
       replyTo: email, // Usando o email diretamente do req.body
       subject: `${name} - via gusttavocastro.com`, // Usando o nome diretamente do req.body
