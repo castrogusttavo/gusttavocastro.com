b4d083cccba6063f7b6f6da0ba160366b1b4d267
castrogusttavo <gusttavoleandro2023@gmail.com>
2024-07-01 19:19:10 +0000

    fix: Add Androme Project in Principal Project

--- pages/projects.js
+++ pages/projects.js
@@ -21,7 +21,7 @@ export async function getStaticProps() {
 
 function Projects(props) {
   const renderFeatured = () => {
-    const featured = ['Therha', 'NVEC', 'Softis', 'Design System']
+    const featured = ['Therha', 'NVEC', 'Softis', 'Andromeda']
 
     return items
       .map(item => {
