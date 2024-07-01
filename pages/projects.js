d7c97bf3f802c5828af183e9865dc660fe797d21
castrogusttavo <gusttavoleandro2023@gmail.com>
2024-07-01 17:57:48 +0000

    fix: Add Project of Design System in Principal Projects

--- pages/projects.js
+++ pages/projects.js
@@ -21,7 +21,7 @@ export async function getStaticProps() {
 
 function Projects(props) {
   const renderFeatured = () => {
-    const featured = ['Therha', 'NVEC', 'Softis', '']
+    const featured = ['Therha', 'NVEC', 'Softis', 'Design System']
 
     return items
       .map(item => {
