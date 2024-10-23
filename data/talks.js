f1aa6e1bdf7aacfe2e2aa812d6e98e97a16fedb7
castrogusttavo <gusttavoleandro2023@gmail.com>
2024-10-23 14:29:16 -0300

    ci: update data in talks page

--- data/talks.js
+++ data/talks.js
@@ -1,19 +1,44 @@
 const items = [
   {
-    year: '2024',
+    year: '2023',
     summary: '',
     talks: [
       {
-        title: 'Epic Web Conf',
-        url: 'https://www.epicweb.dev/talks',
-        cover: '/static/images/talk-epicweb.jpg',
-        date: '2024-04-10',
-        where: 'Park City, Utah - USA',
+        title: 'Developer Experience',
+        url: '',
+        cover: '/static/images/confFigma.jpg',
+        date: '2023-11-23',
+        where: 'Etec Drª Ruth Cardoso, SP - Brazil',
         presentations: [
           {
             title: 'The Next Generation of Developer-First Products',
-            url: 'https://speakerdeck.com/zenorocha/the-next-generation-of-developer-first-products',
-            video: 'https://www.epicweb.dev/talks/the-next-generation-of-developer-first-products'
+            url: '',
+          },
+        ],
+      },
+      {
+        title: 'Hands-on Figma for devs',
+        url: '',
+        cover: '/static/images/talk-epicweb.jpg',
+        date: '2023-06-13',
+        where: 'Etec Drª Ruth Cardoso, SP - Brazil',
+        presentations: [
+          {
+            title: 'First Steps in Prototyping with Figma',
+            url: 'https://www.linkedin.com/posts/francissimoes_figma-ds-ruthinha-activity-7088967349956976640-9ZAa?utm_source=share&utm_medium=member_desktop',
+          },
+        ],
+      },
+      {
+        title: 'Pitch - Startup',
+        url: '',
+        cover: '/static/images/talk-epicweb.jpg',
+        date: '2023-05-25',
+        where: 'Etec Drª Ruth Cardoso, SP - Brazil',
+        presentations: [
+          {
+            title: 'How to Create Your Elevator Pitch',
+            url: 'https://www.linkedin.com/posts/sofia-rodrigues-rocha-430159265_oportunidade-pitch-oficina-activity-7081716944411721728-0ex2?utm_source=share&utm_medium=member_desktop'
           },
         ],
       },
