997eacd54e2a728593a760c4533ae38b9e152859
castrogusttavo <gusttavoleandro2023@gmail.com>
2025-06-01 22:42:56 -0300

    feat: add plausible-tracker package

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,35 @@
 import '../public/static/css/prism.css'
 import 'remixicon/fonts/remixicon.css'
 
+import plausible from 'plausible-tracker'
+
 import Router from 'next/router'
 import * as gtag from '../lib/gtag'
 import CommandBar from '../components/CommandBar'
+import { useRouter} from 'next/router'
+import { useEffect } from 'react'
 
 Router.events.on('routeChangeComplete', url => gtag.pageview(url))
 
 const Noop = ({ children }) => children
 
+const { trackPageview } = plausible({
+  domain: 'castrogusttavo.vercel.app',
+})
+
 export default function MyApp({ Component, pageProps }) {
   const Layout = Component.Layout || Noop
+
+  const router = useRouter()
+
+  useEffect(() => {
+    trackPageview()
+
+    router.events.on('routeChangeComplete', () => {
+      trackPageview()
+    })
+  }, [])
+
 
   return (
     <CommandBar>
